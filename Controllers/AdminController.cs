eb91145d144dad7a9d3fe2a7e198cf05c1187765
Sobira <sobira204@gmail.com>
2020-06-08 11:47:53 +0500

    add addPresent modul

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -28,6 +28,34 @@ namespace PersonalPresents.Controllers
             return View(presetns);
         }
         [HttpGet]
+        public async Task<IActionResult> Add(){
+            ViewBag.Gender = await _context.Genders.ToListAsync();
+            ViewBag.Festival = await _context.Festivals.ToListAsync();
+            ViewBag.Interest = await _context.Interests.ToListAsync();
+            ViewBag.Profession = await _context.Professions.ToListAsync();
+            ViewBag.RoleForUser = await _context.RoleForUsers.ToListAsync();
+            return View();            
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Present p){
+            if(ModelState.IsValid){
+                var gender = await _context.Genders.FirstOrDefaultAsync(x => x.Id == p.GenderId);
+                gender.Presents.Add(p);
+                var role = await _context.RoleForUsers.FirstOrDefaultAsync(x => x.Id == p.RoleId);
+                role.Presents.Add(p);
+                var fest = await _context.Festivals.FirstOrDefaultAsync(x => x.Id == p.FestivalId);
+                fest.Presents.Add(p);
+                var interest = await _context.Interests.FirstOrDefaultAsync(x => x.Id == p.InterestId);
+                interest.Presents.Add(p);
+                var prof = await _context.Professions.FirstOrDefaultAsync(x => x.Id == p.ProfessionId);
+                prof.Presents.Add(p);
+                _context.Presents.Add(p);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("GetAllPresents");
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpGet]
         public async Task<IActionResult> Change(int Id){
             Present p = new Present(){Id = Id};
             ViewBag.Gender = await _context.Genders.ToListAsync();
