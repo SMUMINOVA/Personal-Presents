c1f5fb0bd8b86cd56d3c1a97c38a7cc7a7e8efe2
Sobira <sobira204@gmail.com>
2020-06-08 11:11:25 +0500

    add change modul

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -15,7 +15,7 @@ using PersonalPresents.Models.PresentModels;
 
 namespace PersonalPresents.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
         private AppDbContent _context;
         public AdminController(AppDbContent context)
@@ -26,6 +26,19 @@ namespace PersonalPresents.Controllers
         public async Task<IActionResult> GetAllPresents(){
             var presetns = await _context.Presents.ToListAsync();
             return View(presetns);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Change(int Id){
+            Present p = new Present(){Id = Id};
+            ViewBag.Gender = await _context.Genders.ToListAsync();
+            ViewBag.Festival = await _context.Festivals.ToListAsync();
+            ViewBag.Interest = await _context.Interests.ToListAsync();
+            ViewBag.Profession = await _context.Professions.ToListAsync();
+            ViewBag.RoleForUser = await _context.RoleForUsers.ToListAsync();
+            return View(p);
+        }
+        public IActionResult Change(Present p){
+            
         }
     }
 }
